c5925d7bb40e0e0a3547f67d014e31813a16ae9c
mljes <30917336+mljes@users.noreply.github.com>
2020-10-02 16:22:59 -0300

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Maria J.', // Title for your website.
+    tagline: 'Fifth-year BCS student who would like a job.',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://mljes.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
